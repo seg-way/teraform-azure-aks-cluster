674f39176dae4eb58e5413aed3a08449e082d713
Ryan Faircloth <ryan@dss-i.com>
2023-01-09 15:48:07 -0500

    fix: updates

--- main.tf
+++ main.tf
@@ -55,9 +55,9 @@ module "aks_cluster_name" {
   ingress_application_gateway_subnet_cidr = "10.1.0.0/24"
   local_account_disabled                  = true
 
-  net_profile_dns_service_ip     = "10.0.0.10"
-  net_profile_docker_bridge_cidr = "170.10.0.1/16"
-  net_profile_service_cidr       = "10.0.0.0/16"
+  # net_profile_dns_service_ip     = "10.0.0.10"
+  # net_profile_docker_bridge_cidr = "170.10.0.1/16"
+  # net_profile_service_cidr       = "10.0.0.0/16"
   network_plugin                 = "azure"
   network_policy                 = "azure"
   os_disk_size_gb                = 60
