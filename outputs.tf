a378b8e28a64ccda44272718696b07d2908bac9c
Ryan Faircloth <ryan@dss-i.com>
2023-01-10 16:10:44 -0500

    feat: output admin info

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "name" {
     value = module.aks_cluster_name.aks_name
 }
+output "admin_host" {
+    value = module.aks_cluster_name.admin_host
+}
+output "admin_client_certificate" {
+    value = base64decode(module.aks_cluster_name.admin_client_certificate)
+}
+output "admin_client_key" {
+    value = base64decode(module.aks_cluster_name.admin_client_key)
+}
+output "admin_cluster_ca_certificate" {
+    value = base64decode(module.aks_cluster_name.admin_cluster_ca_certificate)
+}
