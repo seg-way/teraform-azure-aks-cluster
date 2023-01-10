f1620b12b3f219c162c1462b3a0099555e6f5561
Ryan Faircloth <ryan@dss-i.com>
2023-01-10 16:11:45 -0500

    fix: output admin info

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,11 @@ output "admin_host" {
     value = module.aks_cluster_name.admin_host
 }
 output "admin_client_certificate" {
-    value = base64decode(module.aks_cluster_name.admin_client_certificate)
+    value = module.aks_cluster_name.admin_client_certificate
 }
 output "admin_client_key" {
-    value = base64decode(module.aks_cluster_name.admin_client_key)
+    value = module.aks_cluster_name.admin_client_key
 }
 output "admin_cluster_ca_certificate" {
-    value = base64decode(module.aks_cluster_name.admin_cluster_ca_certificate)
+    value = module.aks_cluster_name.admin_cluster_ca_certificate
 }
