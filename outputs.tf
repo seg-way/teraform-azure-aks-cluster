9e4ddddd234fde50ebb608bdf63379bdc62e8ddd
Ryan Faircloth <ryan@dss-i.com>
2023-06-14 08:14:22 -0400

    fix: cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
 output "name" {
-    value = module.aks_cluster_name.aks_name
+  value = module.aks_cluster_name.aks_name
 }
 output "admin_host" {
-    value = module.aks_cluster_name.admin_host
-    sensitive = true
+  value     = module.aks_cluster_name.admin_host
+  sensitive = true
 }
 output "admin_client_certificate" {
-    value = module.aks_cluster_name.admin_client_certificate
-    sensitive = true
+  value     = module.aks_cluster_name.admin_client_certificate
+  sensitive = true
 }
 output "admin_client_key" {
-    value = module.aks_cluster_name.admin_client_key
-    sensitive = true
+  value     = module.aks_cluster_name.admin_client_key
+  sensitive = true
 }
 output "admin_cluster_ca_certificate" {
-    value = module.aks_cluster_name.admin_cluster_ca_certificate
-    sensitive = true
+  value     = module.aks_cluster_name.admin_cluster_ca_certificate
+  sensitive = true
+}
+
+output "endpoint" {
+  value     = module.aks_cluster_name.admin_host
+  sensitive = true
+
+}
+output "ca_certificate" {
+  value     = module.aks_cluster_name.admin_cluster_ca_certificate
+  sensitive = true
 }
