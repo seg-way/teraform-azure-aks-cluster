38922c5ebfc2de9a6b0bcf02ca58f199bb332ab1
Ryan Faircloth <ryan@dss-i.com>
2023-08-24 17:33:50 -0400

    fix: support env for kubelogin

--- versions.tf
+++ versions.tf
@@ -10,5 +10,9 @@ terraform {
       source  = "hashicorp/azuread"
       version = ">= 2.39.0, < 3.0"
     }
+     azapi = {
+      source = "Azure/azapi"
+      version = ">=1.8.0, < 2.0"
+    }
   }
 }
